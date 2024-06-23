538264e7db0ee70d44c354727bba2fe99a15bf06
davirrocha <davirochamoraes7@gmail.com>
2024-06-23 17:44:40 -0300

    V0.7

--- document.js
+++ document.js
@@ -1,19 +1,19 @@
 const convertButton = document.querySelector(".button-convert")
-
-function diceConvert() {
+const convertTo = document.querySelector(".convert-to")
+async function diceConvert() {
     const inputCurrency = document.querySelector(".input-value").value;
     const valueConvertion = document.querySelector("#real")
     const valueConvertionDolar = document.querySelector("#dolar")
-    const realToday = 0.20
-    const dolarToday = 5.11
+    
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const realToday = data.USDBRL.high;
+    const dolarToday = data.EURBRL.high;
     const euroToday = 5.48
     const libraToday = 6.4003
     const yenToday = 0.033
-    const convertFrom = document.querySelector(".convert-from")
-    const convertTo = document.querySelector(".convert-to")
-    const valueTo = document.querySelector(".covert-from")
-    const imgValue = document.querySelector(".eua")
-
+    const bitToday = data.BTCBRL.high;
+    console.log(data)
 
     valueConvertion.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -21,12 +21,6 @@ function diceConvert() {
     }).format(inputCurrency)
 
 
-    if (convertTo.value == "real") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-        }).format(inputCurrency / realToday)
-    }
 
     if (convertTo.value == "dolar") {
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("en-IN", {
@@ -56,11 +50,17 @@ function diceConvert() {
         }).format(inputCurrency / yenToday)
     }
 
+    if (convertTo.value == "btc") {
+        valueConvertionDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputCurrency / bitToday)
+    }
 }
 
 convertButton.addEventListener("click", diceConvert)
 
-const convertTo = document.querySelector(".convert-to")
+
 const nameCurrency = document.querySelector("#usa")
 const imgCurrency = document.getElementById("currency")
 const valueConvertionDolar = document.getElementById("dolar")
@@ -68,36 +68,30 @@ const valueConvertionDolar = document.getElementById("dolar")
 
 convertTo.addEventListener("change", function () {
 
-    // if (convertTo.value == "real") {
-    //     nameCurrency.innerHTML = "Real"
-    //     imgCurrency.src = "./assets/real.png"
-    // }
-
     if (convertTo.value == "dolar") {
         nameCurrency.innerHTML = "Dólar Americano"
         imgCurrency.src = "./assets/dolar.png"
-
     }
 
     if (convertTo.value == "euro") {
         nameCurrency.innerHTML = "EURO"
         imgCurrency.src = "./assets/euro.png"
-
     }
 
     if (convertTo.value == "libra") {
         nameCurrency.innerHTML = "Libra"
         imgCurrency.src = "./assets/libra.png"
-
     }
-
-
 
     if (convertTo.value == "yen") {
         nameCurrency.innerHTML = "Yen"
         imgCurrency.src = "./assets/iene.png"
-
     }
+    if (convertTo.value == "btc") {
+        nameCurrency.innerHTML = "Bitcoin"
+        imgCurrency.src = "./assets/bitcoin.png"
+    }
+
     diceConvert()
 
 })
