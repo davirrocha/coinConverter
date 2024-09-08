6643b7885422ed3086fd87210789037990b29633
davirrocha <davirochamoraes7@gmail.com>
2024-09-08 15:58:22 -0300

    changes

--- document.js
+++ document.js
@@ -1,21 +1,18 @@
-const convertButton = document.querySelector(".button-convert")
-const convertTo = document.querySelector(".convert-to")
+
+//Código para fazer as conversões
 async function diceConvert() {
     const inputCurrency = document.querySelector(".input-value").value;
-    const valueConvertion = document.querySelector("#real")
-    const valueConvertionDolar = document.querySelector("#dolar")
-    
-    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    const conversionValue = document.querySelector("#conversion")
+    const convertedValue = document.querySelector("#converted")
 
-    const realToday = data.USDBRL.high;
-    const dolarToday = data.EURBRL.high;
-    const euroToday = 5.48
-    const libraToday = 6.4003
-    const yenToday = 0.033
-    const bitToday = data.BTCBRL.high;
-    console.log(data)
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL").then(response => response.json())
 
-    valueConvertion.innerHTML = new Intl.NumberFormat("pt-BR", {
+    const dolarToday = data.USDBRL.high;
+    const euroToday = data.EURBRL.high;
+    const libraToday = data.GBPBRL.high;
+
+
+    conversionValue.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrency)
@@ -23,48 +20,33 @@ async function diceConvert() {
 
 
     if (convertTo.value == "dolar") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("en-IN", {
+        convertedValue.innerHTML = new Intl.NumberFormat("en-EN", {
             style: "currency",
             currency: "USD",
         }).format(inputCurrency / dolarToday) //NÃO PODE DEIXAR JUNTOS
     }
 
     if (convertTo.value == "euro") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("nl-BE", {
+        convertedValue.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "EUR",
         }).format(inputCurrency / euroToday)
     }
 
     if (convertTo.value == "libra") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+        convertedValue.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "GBP",
         }).format(inputCurrency / libraToday)
     }
-
-    if (convertTo.value == "yen") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("ja-JP", {
-            style: "currency",
-            currency: "JPY",
-        }).format(inputCurrency / yenToday)
-    }
-
-    if (convertTo.value == "btc") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "BTC",
-        }).format(inputCurrency / bitToday)
-    }
 }
 
-convertButton.addEventListener("click", diceConvert)
 
 
-const nameCurrency = document.querySelector("#usa")
+//Codigo para mudar o estilo
+const convertTo = document.querySelector(".convert-to")
 const imgCurrency = document.getElementById("currency")
-const valueConvertionDolar = document.getElementById("dolar")
-
+const nameCurrency = document.querySelector("#curencyName")
 
 convertTo.addEventListener("change", function () {
 
@@ -74,7 +56,7 @@ convertTo.addEventListener("change", function () {
     }
 
     if (convertTo.value == "euro") {
-        nameCurrency.innerHTML = "EURO"
+        nameCurrency.innerHTML = "Euro"
         imgCurrency.src = "./assets/euro.png"
     }
 
@@ -83,17 +65,7 @@ convertTo.addEventListener("change", function () {
         imgCurrency.src = "./assets/libra.png"
     }
 
-    if (convertTo.value == "yen") {
-        nameCurrency.innerHTML = "Yen"
-        imgCurrency.src = "./assets/iene.png"
-    }
-    if (convertTo.value == "btc") {
-        nameCurrency.innerHTML = "Bitcoin"
-        imgCurrency.src = "./assets/bitcoin.png"
-    }
-
     diceConvert()
-
 })
 
 
